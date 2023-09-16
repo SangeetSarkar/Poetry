c0a24aea4263e49e148cd33828f1cd8a653176e3
Sangeet Sarkar <sangeetsarkar2002@gmail.com>
2023-09-16 22:49:19 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Container from './Components/Container';
+
+function App() {
+  return (
+    <>
+    <div className='parent'>
+      <div className='mainContainer'>
+        <Container></Container>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Container.jsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+
+function Container() {
+  return (
+    <Card align='center' mt={10}>
+      <CardBody>
+        wvih;bvwiabv;wbvilvbeaofvbeoaw;vbesaovbewaijvbeofvefojveaflv eoafsvbefveb afvekvbevbefovbeovbe;ovbwaobv
+      </CardBody>
+    </Card>
+  );
+}
+
+export default Container;
